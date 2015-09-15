bf5831f17ba4c919d855150d40da45b956082509
Denis Rykovanov <drykovanov@wiley.com>
2015-09-15 22:12:05 +0300

    [+] server host and port can be overriden by env properties

--- server/src/main/scala/ServerStarter.scala
+++ server/src/main/scala/ServerStarter.scala
@@ -6,9 +6,13 @@ import com.chaoslabgames.tcpfront.AkkaNetServerTCP
  * Created by Julia on 13.09.2015.
  */
 object ServerStarter extends App {
+
+    val serverHost = System.getProperty("SERVER_HOST", "127.0.0.1")
+    val serverPort = System.getProperty("SERVER_PORT", "8899").toInt
+
     val actorSystem = ActorSystem("akka-server")
     val gameModelService = actorSystem.actorOf(Props(new GameModelService()))
-    val actorNet    = actorSystem.actorOf(AkkaNetServerTCP.props("127.0.0.1", 8899, gameModelService), "tcp-front")
+    val actorNet    = actorSystem.actorOf(AkkaNetServerTCP.props(serverHost, serverPort, gameModelService), "tcp-front")
 }
 
 
